6ab8b54fef6f1a075c2bcf2d59c6183158fe2aae
Oktay Arda <hasanoktayarda@gmail.com>
2018-10-24 14:30:31 +0300

    eslint added

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Testing React Native!</Text>
+        <Text style={styles.welcome}>Live reload testing!!</Text>
       </View>
     );
   }
